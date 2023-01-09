bea3d28894bedde79f687e1f3e1283e61da4fe64
ieongeun <ieongeun0@gmail.com>
2023-01-09 14:51:43 +0900

    무한슬라이더 완성

--- index.js
+++ index.js
@@ -4,21 +4,40 @@ const slide = document.querySelectorAll('#slider-wrap .slide');
 const btns = document.querySelectorAll('#slider-wrap .slider-btn-wrap');
 
 let slideIdx = 0;
-let lastSlideIdx = slide.length -1;
+
+firstSilde = slide[0];
+lastSilde = slide[slide.length-1];
+
+firstSildeNode = firstSilde.cloneNode(true);
+lastSildeNode = lastSilde.cloneNode(true);
+
+firstSilde.before(lastSildeNode);
+lastSilde.after(firstSildeNode);
+
 
 for (const btn of btns) {
     btn.addEventListener('click',function name(e) {
-        moveSlide(e.target.className);
+        e.target.className == 'next' ? moveSlide(1): moveSlide(-1);
     })
 }
 
+let slideLength = slide.length;
+let clonedSlideLength = slide.length + 2;
+let firstSildeIdx = 1;
+let lastSlideIdx = slide.length;
+let slideWidth = window.innerWidth;
+
 function moveSlide(direction) {
-    if (direction == 'next') {
-        slideIdx = (slideIdx === lastSlideIdx) ? 0 : ++slideIdx;
-        console.log(slideIdx);
-    } else {
-        slideIdx = (slideIdx === 0) ? lastSlideIdx : --slideIdx;
-        console.log(slideIdx);
+    slideIdx += direction;
+    slider.scroll({left:(slideWidth * slideIdx), behavior:'smooth'});
+    if (slideIdx == 0 || slideIdx == clonedSlideLength - 1) {
+        if (slideIdx == 0 ) {
+            slideIdx = lastSlideIdx;
+        } else {
+            slideIdx = firstSildeIdx;
+        }
+        setTimeout(function name(params) {
+            slider.scrollLeft = (slideWidth * slideIdx);
+        },500)
     }
-    slider.scroll({left:(window.innerWidth * slideIdx), behavior:'smooth'});
 }

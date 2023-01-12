1212f7cf31332a526eea36dabaf9841da2ad6152
ieongeun <ieongeun0@gmail.com>
2023-01-12 13:34:32 +0900

    페이지네이션 동적으로 생성 코드 추가

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ for (const btn of btns) {
 let slideLength = slide.length;
 let clonedSlideLength = slide.length + 2;
 let firstSildeIdx = 1;
-let lastSlideIdx = slide.length;
+let lastSlideIdx = slideLength;
 let slideWidth = window.innerWidth;
-let slideIdx = 4;
+let slideIdx = 4; // 이거 하드코딩이다 
 slider.scrollLeft = (slideWidth * slideIdx);
 
 function moveSlide(direction) {
@@ -50,10 +50,40 @@ function moveSlide(direction) {
     }
 }
 
-
 window.addEventListener('resize',function () {
     slideWidth = window.innerWidth;
     slider.scroll({left:(slideWidth * slideIdx), behavior:'smooth'});
 })
 
 
+    let startX;
+    let endX;
+    let diffX;
+
+    slider.addEventListener('touchstart',function name(e) {
+        startX = e.touches[0].clientX;
+    })
+
+    slider.addEventListener('touchend',function name(e) {
+        endX = e.changedTouches[0].clientX;
+        diffX = startX - endX;
+        if(Math.abs(diffX)>= window.innerWidth / 2) {
+            (diffX > 0) ? moveSlide(1): moveSlide(-1);
+        }else{
+            slider.scroll({left:(slideWidth * slideIdx), behavior:'smooth'})
+        }
+    })
+
+    slider.addEventListener('touchmove',function name(e) {
+        moveX = e.touches[0].clientX;
+        diffX = startX -moveX;
+        slider.scrollLeft = (slideWidth * slideIdx)+ diffX;
+    });
+
+    // 페이지네이션
+    const pagination = document.querySelector('#slider-wrap ul.pagination-wrap');
+    for (let i = 0; i < slideLength; i++) {
+        const paginationItem = document.createElement('li');
+        pagination.appendChild(paginationItem);
+    }
+    // paginationItem.setAttribute('','');
